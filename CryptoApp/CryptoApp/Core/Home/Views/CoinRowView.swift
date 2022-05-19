20d42f17a1d26f0fa7a7e7dc61002bb9ddc91ff1
borisbolsh <bolsh.bo@yandex.ru>
2022-05-19 23:49:17 +0300

    currrrency formatter

--- CryptoApp/CryptoApp/Core/Home/Views/CoinRowView.swift
+++ CryptoApp/CryptoApp/Core/Home/Views/CoinRowView.swift
@@ -53,24 +53,24 @@ extension CoinRowView {
 
     private var centerColumn: some View {
         VStack(alignment: .trailing) {
-//            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
-//                .bold()
-//            Text((coin.currentHoldings ?? 0).asNumberString())
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                .bold()
+            Text((coin.currentHoldings ?? 0).asNumberString())
         }
         .foregroundColor(Color.theme.accent)
     }
 
     private var rightColumn: some View {
         VStack(alignment: .trailing) {
-//            Text(coin.currentPrice.asCurrencyWith6Decimals())
-//                .bold()
-//                .foregroundColor(Color.theme.accent)
-//            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
-//                .foregroundColor(
-//                    (coin.priceChangePercentage24H ?? 0 >= 0) ?
-//                    Color.theme.green :
-//                    Color.theme.red
-//                )
+            Text(coin.currentPrice.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundColor(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundColor(
+                    (coin.priceChangePercentage24H ?? 0 >= 0) ?
+                    Color.theme.green :
+                    Color.theme.red
+                )
         }
         .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
     }
